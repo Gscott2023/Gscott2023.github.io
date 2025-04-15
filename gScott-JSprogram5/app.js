aff5523063852cc73f4ea2e480ee252d46a80849
Gscott2023 <gscott0057@kctcs.edu>
2025-04-15 10:42:35 -0400

    edit JSprogram5

--- gScott-JSprogram5/app.js
+++ gScott-JSprogram5/app.js
@@ -7,7 +7,7 @@ async function getData(selected_major) {
         // filter data array for the selected meal
         major_items = data.filter( (item) => item.major == selected_major );  
 
-       var templateText = document.getElementById('menuTemplate').innerHTML;
+       var templateText = document.getElementById('majorTemplate').innerHTML;
        var compiledTemplateText = Handlebars.compile(templateText);   // get and compile template code
        compiledHtml = compiledTemplateText({ rows: major_items })      // apply data to template
        document.getElementById('studentTable').innerHTML = compiledHtml; 
